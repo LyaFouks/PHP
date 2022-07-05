db750e8e48fd21d1b49e470be71a450a77a224fe
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-07-05 07:11:58 +0300

    Переменные

--- hello.php
+++ hello.php
@@ -1,3 +1,16 @@
 <?php 
-echo "Hello PHP!"; 
+ 
+
+$name = "John";
+$lastname = "Black";
+echo $name." ".$lastname;
+$x = 23;
+$y = 21;
+$sum = $x + $y + 3 / 6;
+echo "<p>$sum</p>";
+$bool = true;
+echo "<p>$bool</p>";
+$variableName = "lastname";
+echo $$variableName;
+
 ?>
