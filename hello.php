4372186b2cfa636007b8cf297f8e6d5fb1aea38d
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-07-05 07:21:21 +0300

    Arrays

--- hello.php
+++ hello.php
@@ -1,16 +1,33 @@
 <?php 
- 
 
-$name = "John";
-$lastname = "Black";
-echo $name." ".$lastname;
-$x = 23;
-$y = 21;
-$sum = $x + $y + 3 / 6;
-echo "<p>$sum</p>";
-$bool = true;
-echo "<p>$bool</p>";
-$variableName = "lastname";
-echo $$variableName;
+$colors =  array ("red", "orange", "blue", "green");
+$colors[] = "yellow";
+print_r($colors);
+
+echo $colors[1];
+
+echo "<br>";
+
+$names[0] = "Jane";
+$names[1] = "Jim";
+$names[5] = "Jack";
+$names["name"] = "Joan";
+
+unset($names[5]);
+echo sizeof($names);
+
+print_r($names);
+
+echo "<br>";
+
+echo $names["name"];
+
+$years["Jane"] = 1999;
+$years["Jim"] = 1979;
+$years["Jack"] = 2001;
+
+echo "<br>";
+
+print_r($years);
 
 ?>
