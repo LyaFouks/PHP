6de65fbad684c83244053e5aa0167168a9c32e60
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-07-05 18:35:22 +0300

    Get

--- hello.php
+++ hello.php
@@ -1,24 +1,13 @@
 <?php 
-
-$x = 7;
-$i = 1;
-
-while ($i <= 10) {
-	$y = $x * $i;
-	echo $y."<br>";
-
-	$i++;
-}
-
-$colors = array("red", "orange", "green", "blue", "yellow", "indigo", "violet");
-
-$j = 0;
-
-while ($j < sizeof($colors)) {
-	echo $colors[$j]."<br>";
-
-	$j++;
-
-}
-
+	print_r($_GET);
+	echo "Hello ".$_GET["userName"]."!";
 ?>
+
+<p>Input your name</p>
+
+<form>
+	
+	<input type="text" name="userName">
+	<input type="submit" name="">
+
+</form>
