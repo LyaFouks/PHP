591b1a0a29e74e572d46923943fb3e4d36153567
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-07-05 07:29:22 +0300

    If

--- hello.php
+++ hello.php
@@ -1,33 +1,13 @@
 <?php 
 
-$colors =  array ("red", "orange", "blue", "green");
-$colors[] = "yellow";
-print_r($colors);
+$userName = "Joe";
+$userAge = 20;
+$isUserLogin = true;
 
-echo $colors[1];
-
-echo "<br>";
-
-$names[0] = "Jane";
-$names[1] = "Jim";
-$names[5] = "Jack";
-$names["name"] = "Joan";
-
-unset($names[5]);
-echo sizeof($names);
-
-print_r($names);
-
-echo "<br>";
-
-echo $names["name"];
-
-$years["Jane"] = 1999;
-$years["Jim"] = 1979;
-$years["Jack"] = 2001;
-
-echo "<br>";
-
-print_r($years);
+if($isUserLogin || $userAge > 18) {
+	echo "Hello ".$userName;
+} else {
+	echo "You have to login and you have to be older than 18";
+}
 
 ?>
