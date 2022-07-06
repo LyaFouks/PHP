65e9f2a953375a3f154a0a758a443c8b3dc524bf
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-07-06 07:47:37 +0300

    GET Test

--- hello.php
+++ hello.php
@@ -1,13 +1,32 @@
 <?php 
-	print_r($_GET);
-	echo "Hello ".$_GET["userName"]."!";
+
+	if(isset($_GET["number"])) {
+    	$value = $_GET['number'];
+
+		if(floor($value) == $value && is_numeric($value)) {
+
+		if ($value % 2) {
+		  echo $value.' is odd';
+		} else {
+		  echo $value.' is even';
+		}
+		
+		} else {
+			echo("Input the whole number.");
+}
+	
+
+	}
+
+	
+
 ?>
 
-<p>Input your name</p>
+<p>Input whole number</p>
 
 <form>
 	
-	<input type="text" name="userName">
+	<input type="text" name="number">
 	<input type="submit" name="">
 
 </form>
